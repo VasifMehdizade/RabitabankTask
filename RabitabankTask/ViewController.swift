6261709d018dabc22d36e3c12523d722c6962187
VasifMehdizade <109298511+VasifMehdizade@users.noreply.github.com>
2023-08-11 14:30:57 +0400

    Initial commit

---
+++ RabitabankTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RabitabankTask
+//
+//  Created by Vasif Mehdi on 11.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
